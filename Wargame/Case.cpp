28d1327429173ad968f5694205e1b9d087b6ae96
Jalaliel <chloe.decultot@gmail.com>
2017-04-28 09:30:46 +0200

    test

---
+++ Wargame/Case.cpp
@@ -0,0 +1,18 @@
+/*
+ * Case.cpp
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: cdecultot
+ */
+
+#include "Case.hpp"
+
+Case::Case() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Case::~Case() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Wargame/Case.hpp
@@ -0,0 +1,17 @@
+/*
+ * Case.hpp
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: cdecultot
+ */
+
+#ifndef CASE_HPP_
+#define CASE_HPP_
+
+class Case {
+public:
+	Case();
+	virtual ~Case();
+};
+
+#endif /* CASE_HPP_ */
