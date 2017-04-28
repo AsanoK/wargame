07d5804831eab3e42abfe295699f1b2dcb03366c
Hugin <Hugin@172.18.23.45>
2017-04-28 10:44:35 +0200

    màj unité

---
+++ src/Fantassin.cpp
@@ -0,0 +1,19 @@
+/*
+ * Fantassin.cpp
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: Hugin
+ */
+#include "Fantassin.hpp"
+using namespace Unite;
+bool isCaseAtteignable(Case cible ) {
+	bool ret = false;
+
+	return ret;
+}
+bool isCaseAttaquable(Case cible) {
+	bool ret = false;
+
+	return ret;
+}
+
