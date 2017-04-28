9742f4db81bfc32c2557ba5d0b8424e939ca70e3
Jalaliel <chloe.decultot@gmail.com>
2017-04-28 11:21:27 +0200

    Début de Plateau

---
+++ src/Plateau.cpp
@@ -0,0 +1,18 @@
+/*
+ * Plateau.cpp
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: cdecultot
+ */
+
+#include "Plateau.hpp"
+
+Plateau::Plateau() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Plateau::~Plateau() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Plateau.hpp
@@ -0,0 +1,22 @@
+/*
+ * Plateau.h
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: cdecultot
+ */
+
+
+#ifndef PLATEAU_HPP_
+#define PLATEAU_HPP_
+#define NBCASES 50
+class Plateau {
+private:
+	Case cases[NBCASES][NBCASES];
+	void genererGrilleAleatoire();
+	void deployerUnitesInitiales(Joueur)
+public:
+	Plateau();
+	virtual ~Plateau();
+};
+
+#endif /* PLATEAU_HPP_ */
