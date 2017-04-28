94b043d216c4abc897a84a37fedcc65caaf1c300
Hugin <Hugin@172.18.23.45>
2017-04-28 09:12:31 +0200

    classe unité

---
+++ src/Fantassin.hpp
@@ -0,0 +1,15 @@
+/*
+ * Fantassin.hpp
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: Hugin
+ */
+
+#ifndef FANTASSIN_HPP_
+#define FANTASSIN_HPP_
+
+
+
+
+
+#endif /* FANTASSIN_HPP_ */
---
+++ src/Unite.hpp
@@ -0,0 +1,15 @@
+/*
+ * Unite.hpp
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: Hugin
+ */
+
+#ifndef UNITE_HPP_
+#define UNITE_HPP_
+
+
+
+
+
+#endif /* UNITE_HPP_ */
